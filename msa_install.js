04b73f255bb14755a81f044c7caa2cec468a6279
Nicolas CARREZ <nicolas.carrez@clustree.com>
2020-02-08 15:23:17 +0100

    Fix msa_ideas schema

--- msa_install.js
+++ msa_install.js
@@ -3,16 +3,16 @@ module.exports = async itf => {
 	const { withDb } = Msa.require('db')
 	await withDb(async db => {
 		await db.run(
-		`CREATE TABLE IF NOT EXISTS msa_idea_sets (
-			id VARCHAR(255) PRIMARY KEY,
-			params TEXT
-		)`)
+			`CREATE TABLE IF NOT EXISTS msa_idea_sets (
+				id VARCHAR(255) PRIMARY KEY,
+				params TEXT
+			)`)
 		await db.run(
 			`CREATE TABLE IF NOT EXISTS msa_ideas (
 				id VARCHAR(255),
 				num INTEGER,
 				parent INTEGER,
-				text TEXT,
+				content TEXT,
 				createdBy VARCHAR(255),
 				updatedBy VARCHAR(255),
 				PRIMARY KEY (id, num)
